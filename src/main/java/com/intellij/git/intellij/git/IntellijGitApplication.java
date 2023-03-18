3ece3b818edb6bb557382f1e29c4564ce091574d
JagadheeshRayudu <jagadheeshrayudu@gmail.com>
2023-03-18 12:52:34 +0530

    initial commit

---
+++ src/main/java/com/intellij/git/intellij/git/IntellijGitApplication.java
@@ -0,0 +1,13 @@
+package com.intellij.git.intellij.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intellij/git/intellij/git/IntellijGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellij.git.intellij.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
